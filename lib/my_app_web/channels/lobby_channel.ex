c5856853c34384dbaa9d0cb28b24e4e1be1de574
Ruslan Savenok <ruslan.savenok@gmail.com>
2018-07-17 11:53:46 +0200

    Initial channel events

--- lib/my_app_web/channels/lobby_channel.ex
+++ lib/my_app_web/channels/lobby_channel.ex
@@ -2,6 +2,22 @@ defmodule MyAppWeb.LobbyChannel do
   use MyAppWeb, :channel
 
   def join("lobby", payload, socket) do
-    {:ok, socket}
+    user_id =
+      :crypto.strong_rand_bytes(10)
+      |> Base.url_encode64
+
+    send(self, :after_join)
+
+    {:ok, assign(socket, :user_id, user_id)}
+  end
+
+  def handle_info(:after_join, socket) do
+    broadcast socket, "updated", %{count: 0}
+    {:noreply, socket}
+  end
+
+  def terminate(_reason, socket) do
+    broadcast socket, "updated", %{count: 0}
+    :ok
   end
 end
